9811d9fd50b1f2ad1659008e850319e792a41907
Derya Aydede <derya.aydede@gmail.com>
2016-07-14 16:51:52 -0700

    added validations to models

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,5 +1,10 @@
 class Rent < ActiveRecord::Base
   belongs_to :tenant
   belongs_to :apartment
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :month, presence: true
+  validates :pay_status, presence: true
+  validates_associated :tenants
+  validates_associated :apartments
 
 end
